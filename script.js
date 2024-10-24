3a06610f8b4572485076935545703cc7c074ec96
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2024-10-24 20:58:11 -0300

    end the js code

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ form.addEventListener("submit", (event) => {
   const numeroDeMeses = document.getElementById("numeroDeMeses").value;
   const nomeInvestidor = document.getElementById("nomeInvestidor").value;
 
-  const juros = capitalInicial * taxaDeJuros * numeroDeMeses;
+  let juros = parseFloat(taxaDeJuros / 100);
+  let meses = parseInt(numeroDeMeses);
+  let capital = parseFloat(capitalInicial);
 
-  alert(`sua renda sera de ${juros}`);
+  let montante = capital * Math.pow(1 + juros, meses);
+  alert(
+    ` ola investidor ${nomeInvestidor} seu investimento de ${new Intl.NumberFormat(
+      "pt-BR",
+      { style: "currency", currency: "BRL" }
+    ).format(capital)} com o rendimento de ${
+      juros * 100
+    }% por mês rendeu ${new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(montante.toFixed(2))}`
+  );
 });
